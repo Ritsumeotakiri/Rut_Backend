072cf25320c7c331c086a765624a5333c55a0223
Ritsumeotakiri <141367098+Ritsumeotakiri@users.noreply.github.com>
2025-05-16 23:27:06 +0700

    Update Participant.js

--- models/Participant.js
+++ models/Participant.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const ParticipantSchema = new mongoose.Schema({
-  bibNumber: String,
-  name: String,
+  bibNumber: { type: String, required: true },
+  name: { type: String, required: true },
+  raceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Race', required: false }, // optional
 });
 
 module.exports = mongoose.model('Participant', ParticipantSchema);
